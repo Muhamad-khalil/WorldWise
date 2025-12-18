7e22f7f5828e863a499272ff79d15aa15a6e3e5d
Muhamad-khalil <medo.khalil.01.18@gmail.com>
2025-12-18 20:16:38 +0200

    project WorldWise

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Home from "./pages/Home";
+import Pricing from "./pages/Pricing";
+import PageNotFound from "./pages/PageNotFound";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="home" element={<Home />} />
+        <Route path="" element={<Home />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import PageNav from "../component/PageNav";
+
+function PageNotFound() {
+  return (
+    <div>
+      <PageNav /> Not Found : (
+    </div>
+  );
+}
+
+export default PageNotFound;
